ebb18494552c851e74e225f9b2a238025e950909
Nivas Sanmugam <nivassanmugam@Mac-142.lan>
2026-08-09 01:46:28 +0530

    Revert basePath back to /Nivas.fpv for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/Nivas.fpv",
   // Image optimization
   images: {
     unoptimized: true,
