c831efbf6993646ea8f54b7d23f9d478cbeaeaa7
Nivas Sanmugam <nivassanmugam@Mac-142.lan>
2026-08-09 01:51:02 +0530

    Remove basePath to support custom domain nivasfpv.in

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Nivas.fpv",
   // Image optimization
   images: {
     unoptimized: true,
